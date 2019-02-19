9b6b887a2f33f3bfa1760bea9420351daf5a9001
Matthieu Dorier <mdorier@anl.gov>
2019-02-19 05:58:34 -0600

    corrected the way pybind11 is found

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 from distutils.core import setup
 from distutils.extension import Extension
 from distutils.sysconfig import get_config_vars
+import pybind11
 import pkgconfig
 import os
 import os.path
 import sys
+
+def get_pybind11_include():
+    path = os.path.dirname(pybind11.__file__)
+    return '/'.join(path.split('/')[0:-4] + ['include'])
 
 (opt,) = get_config_vars('OPT')
 os.environ['OPT'] = " ".join(
@@ -17,6 +22,7 @@ server_library_dirs = pk['library_dirs']
 server_library_dirs = pk['library_dirs']
 server_include_dirs = pk['include_dirs']
 server_include_dirs.append(".")
+server_include_dirs.append(get_pybind11_include())
 
 pysdskv_server_module = Extension('_pysdskvserver', ["pysdskv/src/server.cpp"],
 		           libraries=server_libraries,
@@ -31,6 +37,7 @@ client_library_dirs = pk['library_dirs']
 client_library_dirs = pk['library_dirs']
 client_include_dirs = pk['include_dirs']
 client_include_dirs.append(".")
+client_include_dirs.append(get_pybind11_include())
 
 pysdskv_client_module = Extension('_pysdskvclient', ["pysdskv/src/client.cpp"],
 		           libraries=client_libraries,
@@ -39,7 +46,7 @@ pysdskv_client_module = Extension('_pysdskvclient', ["pysdskv/src/client.cpp"],
                    extra_compile_args=['-std=c++11'],
                    depends=["pysdskv/src/client.cpp"])
 setup(name='pysdskv',
-      version='0.1',
+      version='0.2.1',
       author='Matthieu Dorier',
       description="""Python binding for SDSKV""",      
       ext_modules=[ pysdskv_server_module, pysdskv_client_module ],
